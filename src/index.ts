beef9bf7fecde6c2260d052f9632cc3af65ac34c
Lucas Mota <lucas.d.mota@gmail.com>
2021-11-27 01:12:20 -0300

    conserta json, refina condicões

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-const badWords = require('./badWords.json').words
+const badWords = require("./badWords").words
 
 function preventableWord (word) {
-  if(!(typeof word === 'string' || word instanceof String)) throw 'É necessário que o parametro seja um String'
+  if(!(typeof word === "string" || word instanceof String)) throw "É necessário que o parametro seja um String"
+  if(!(word.length < 1)) return false;
   
   const revisedString = reviewFullString(word)
 
   for (const phrase of revisedString) {
     if(phrase.length < 2) continue;
-    if(badWords.indexOf(value) > -1) return true;
+    if(badWords.indexOf(phrase) > -1) return true;
   }
   return false
 }
 
 function reviewFullString (phrase) {
-  if (!phrase) {
-    return ''
-  }
+  
   const lowerCasePhrase = phrase.toLowerCase()
 
   const wordArr = lowerCasePhrase.trim().split(/\s+/)
